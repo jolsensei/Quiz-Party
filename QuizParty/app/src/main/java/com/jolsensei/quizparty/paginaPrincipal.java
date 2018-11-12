2dbdf48e7eebba0f231a5d41cb6d997fc2db99cc
jolsensei <horusensei@gmail.com>
2018-11-12 21:14:28 +0100

    Intento de layout, no termina de colocarse bien

---
+++ QuizParty/app/src/main/java/com/jolsensei/quizparty/paginaPrincipal.java
@@ -0,0 +1,16 @@
+package com.jolsensei.quizparty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class paginaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_principal);
+    }
+}
