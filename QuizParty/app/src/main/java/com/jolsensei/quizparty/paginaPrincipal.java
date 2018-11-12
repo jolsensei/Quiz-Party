5b656ea55019fce57bb13ef09d42d5a210aa945c
jolsensei <horusensei@gmail.com>
2018-11-12 22:09:11 +0100

    No se cuantas veces he rehecho esto, pero me cago en todo lo cagable

--- QuizParty/app/src/main/java/com/jolsensei/quizparty/paginaPrincipal.java
+++ QuizParty/app/src/main/java/com/jolsensei/quizparty/paginaPrincipal.java
@@ -10,7 +10,11 @@ public class paginaPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pagina_principal);
+        this.setContentView(R.layout.activity_pagina_principal);
     }
 }
